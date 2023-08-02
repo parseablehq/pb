3ff0f1049993bf334315706059192448d44ab6f2
Satyam Singh <trueleo@protonmail.com>
2023-08-01 22:29:55 -0800

    Customize error when config not found

--- main.go
+++ main.go
@@ -15,7 +15,11 @@ var DefaultProfile config.Profile
 func PreRunDefaultProfile(cmd *cobra.Command, args []string) error {
 	conf, err := config.ReadConfigFromFile()
 	if err != nil {
-		return err
+		if os.IsNotExist(err) {
+			return errors.New("No config found to run this command. Add a profile using pb profile command")
+		} else {
+			return err
+		}
 	}
 	if conf.Profiles == nil || conf.Default_profile == "" {
 		return errors.New("no profile is configured to run this command. please create one using profile command")
