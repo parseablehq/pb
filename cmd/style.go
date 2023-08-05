7cf91270e42991033dd18f8e6b219b50249b37d2
Satyam Singh <trueleo@protonmail.com>
2023-08-05 11:16:52 +0530

    Add style

--- cmd/style.go
+++ cmd/style.go
@@ -18,20 +18,19 @@
 package cmd
 
 import (
-	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/lipgloss"
 )
 
 // styling for cli outputs
 var (
-	selectedStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.AdaptiveColor{Light: "4", Dark: "11"}).Bold(true)
+	adaptiveYellow = lipgloss.AdaptiveColor{Light: "4", Dark: "11"}
+
+	inactiveStyle    = lipgloss.NewStyle().Faint(true)
+	selectedStyle    = lipgloss.NewStyle().Foreground(adaptiveYellow).Bold(true)
+	selectedStyleAlt = lipgloss.NewStyle().Foreground(adaptiveYellow)
+
+	selectedItemOuter = lipgloss.NewStyle().BorderStyle(lipgloss.NormalBorder()).BorderLeft(true).PaddingLeft(1).BorderForeground(adaptiveYellow)
+	itemOuter         = lipgloss.NewStyle().PaddingLeft(1)
+
 	styleBold = lipgloss.NewStyle().Bold(true)
 )
-
-func listingTableStyle() table.Styles {
-	s := table.DefaultStyles()
-	s.Header = s.Header.Border(lipgloss.NormalBorder(), false, false, true, false)
-	s.Selected = selectedStyle
-	return s
-}
