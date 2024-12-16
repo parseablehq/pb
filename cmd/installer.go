a9969412d777a07ff478d5cc4d639e4260f9c5fd
AdheipSingh <adheip1222@gmail.com>
2024-12-16 21:25:03 +0530

    revert installer refactor

--- cmd/installer.go
+++ cmd/installer.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/briandowns/spinner"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -112,19 +111,6 @@ var InstallOssCmd = &cobra.Command{
 
 		return nil
 	},
-}
-
-// loadParseableConfig loads the configuration from the specified file
-func loadParseableConfig(path string) (*installer.ValuesHolder, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	var config installer.ValuesHolder
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		return nil, err
-	}
-	return &config, nil
 }
 
 // printSuccessBanner remains the same as in the original code
